6c55e392d0c2889f688f5f8847f0e4b69e53e605
Natavan Hasanova <natavanhesenova4@gmail.com>
2022-08-02 18:25:37 +0400

    Login and Register Commands added.

--- ParticipantPanel/ParticipantPanel/UI/Program.cs
+++ ParticipantPanel/ParticipantPanel/UI/Program.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using AuthenticationWithClie.ApplicationLogic;
+using AuthenticationWithClie.Database.Models;
+using AuthenticationWithClie.Database.Repository;
+using System;
+using System.Collections.Generic;
 
-namespace ParticipantPanel
+namespace AuthenticationWithClie.UI
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            Console.WriteLine("Commands :");
+            Console.WriteLine("/register");
+            Console.WriteLine("/login");
+            Console.WriteLine("/exit");
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.Write("Enter command : ");
+                string command = Console.ReadLine();
+
+                if (command == "/register")
+                {
+                    Authentication.Register();
+                }
+                else if (command == "/login")
+                {
+                    Authentication.Login();
+                }
+                else if (command == "/exit")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Command not found!");
+                }
+            }
         }
     }
 }
